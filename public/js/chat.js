ad752a118b430e5fb1be16ae76744110182dcea7
danny <danixeka@gmail.com>
2019-04-07 17:21:49 +1000

    disabling forms and buttons when sending data

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,16 +4,28 @@ const socket = io()
 
 // client (emit) -> server (receive) --acknowledgement --> client
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    
     const message = e.target.elements.message.value
     
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error);
         }
@@ -22,18 +34,45 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+    
     if (!navigator.geolocation) {
+        $sendLocationButton.removeAttribute('disabled')
         return alert('Geolocation is not supported by your browser')
     }
 
-    navigator.geolocation.getCurrentPosition( position => {
+    navigator.geolocation.getCurrentPosition( (position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
             console.log('Location Shared');
+            $sendLocationButton.removeAttribute('disabled')
         })
+    },
+    (error) => {
+        console.log(error);
+        $sendLocationButton.removeAttribute('disabled')
     })
 })
 
+// $sendLocationButton.addEventListener('click', () => {
+//     $sendLocationButton.setAttribute('disabled', 'disabled')
+
+//     if (!navigator.geolocation) {
+//         $sendLocationButton.removeAttribute('disabled')
+//         return alert('Geolocation is not supported by your browser')
+//     }
+ 
+//     navigator.geolocation.getCurrentPosition(
+//         position => {
+//             console.log(position);
+//             $sendLocationButton.removeAttribute('disabled')
+//         },
+//         error => {
+//             console.log(error);
+//             $sendLocationButton.removeAttribute('disabled')
+//         })
+// })
